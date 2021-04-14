7163e464b53bbfc42c37c8fce1c1b86977c96e00
maletinchess <maletinchess2017@gmail.com>
2021-04-14 22:35:30 +0700

    basic set

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const getRss = async (url) => {
+  const response = await axios.get(url);
+  return response.data;
+};
+
+const elements = {
+  form: document.querySelector('form'),
+  btn: document.querySelector('button'),
+  input: document.querySelector('#inputUrl'),
+};
+
+const app = () => {
+  elements.form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const url = formData.get('name');
+
+    try {
+      const data = await getRss(url);
+      const newDiv = document.createElement('div');
+      elements.form.append(newDiv);
+      newDiv.textContent = data;
+    } catch (err) {
+      const newDiv = document.createElement('div');
+      elements.form.append(newDiv);
+      newDiv.textContent = err;
+    }
+  });
+};
+
+export default app;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'template.html',
+    }),
+  ],
+};
