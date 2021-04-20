1bb3085b3508aef33fbc527b93c1c042b9e207de
maletinchess <maletinchess2017@gmail.com>
2021-04-20 23:03:37 +0700

    check parser-3

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 const parseXml = (xmlString) => {
   const parser = new DOMParser();
-  const docXml = parser.parseFromString(xmlString, 'xml');
+  const docXml = parser.parseFromString(xmlString, 'text/xml');
   console.log(docXml);
   const itemsNode = docXml.getElementsByTagName('item');
   const channelNode = docXml.getElementsByTagName('channel');
