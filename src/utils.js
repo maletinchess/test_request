16e5cebe2d8674ae67af39243cf232718997f30c
maletinchess <maletinchess2017@gmail.com>
2021-04-22 01:05:13 +0700

    console reduce data

--- src/utils.js
+++ src/utils.js
@@ -2,13 +2,22 @@ const parseXml = (xmlString) => {
   const parser = new DOMParser();
   const docXml = parser.parseFromString(xmlString, 'text/xml');
   const itemsNode = docXml.getElementsByTagName('item');
-  console.log(
-    [...itemsNode]
-      .map((item) => [...item.children]),
-  );
+
+  const reduceData = (collection) => {
+    const cb = (item, acc) => {
+      const key = item.nodeName;
+      const value = item.textContent;
+      return { ...acc, [key]: value };
+    };
+    return collection.reduce(cb, {});
+  };
+
+  const collection = [...itemsNode].map((item) => [...item.children]);
+
+  console.log(reduceData(collection));
+
   const feedTitle = docXml.getElementsByTagName('title')[0].textContent;
   const feedDescription = docXml.getElementsByTagName('description')[0].textContent;
-  console.log([feedTitle, feedDescription]);
   const id = feedTitle;
   const items = Object.values(itemsNode);
   const parsedItems = items
