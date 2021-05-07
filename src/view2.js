80bac83031f200124c32a6470db7ce52c504de6f
maletinchess <maletinchess2017@gmail.com>
2021-05-07 11:29:25 +0700

    console buttons

--- src/view2.js
+++ src/view2.js
@@ -65,7 +65,7 @@ const renderPosts = (posts, elements) => {
     const modal = buildModalElement(post, post.id);
     ul.append(modal);
   });
-  console.log(elements.modalDiv);
+  console.log(elements.modalButtons);
 };
 
 const renderForm = (dataProcess, elements) => {
