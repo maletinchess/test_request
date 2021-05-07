b40af056ac5c2708c094185fa37076a02b0a4d7c
maletinchess <maletinchess2017@gmail.com>
2021-05-07 12:48:06 +0700

    console posts

--- src/view2.js
+++ src/view2.js
@@ -65,7 +65,7 @@ const renderPosts = (posts, elements) => {
     const modal = buildModalElement(post, post.id);
     ul.append(modal);
   });
-  console.log(elements.modalButtons);
+  console.log(elements.posts);
 };
 
 const renderForm = (dataProcess, elements) => {
