a30bcc057a2eec909b5f2946dd37e7f7a6279783
maletinchess <maletinchess2017@gmail.com>
2021-05-08 12:11:54 +0700

    try condition posts update

--- src/app2.js
+++ src/app2.js
@@ -40,7 +40,9 @@ const updater = (state) => {
     state.feeds = [newFeed, ...filteredFeeds].sort((a, b) => b.id > a.id);
     const receivedPostsWithId = data.posts.map((post) => ({ ...post, id }));
     const diff = _.differenceWith(posts, receivedPostsWithId, _.isEqual);
-    state.posts = [...receivedPostsWithId, ...diff].sort((a, b) => b.id > a.id);
+    if (diff.length > 0) {
+      state.posts = [...receivedPostsWithId, ...diff].sort((a, b) => b.id > a.id);
+    }
   });
 
   setTimeout(() => {
